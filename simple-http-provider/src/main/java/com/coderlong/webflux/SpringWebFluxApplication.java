774818e3eb939c9b5cde056dbff5a0142f49c1ab
rensailong <rensailong@kuaishou.com>
2020-11-19 15:34:35 +0800

    remove nacos

--- simple-http-provider/src/main/java/com/coderlong/webflux/SpringWebFluxApplication.java
+++ simple-http-provider/src/main/java/com/coderlong/webflux/SpringWebFluxApplication.java
@@ -2,13 +2,11 @@ package com.coderlong.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author sailongren
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringWebFluxApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringWebFluxApplication.class, args);
